e39d0ecd9a4f086378816943eda0f20deb5f8ab0
Alex Handley <smithvillescholarsah@gmail.com>
2023-12-20 15:53:14 -0600

    S6M4 Project completed

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  h2 {
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+    font-size: 30px;
+    color: #040093;
+  }
+  figure {
+    border: 1px solid grey;
+    align-content: center;
+
+    img {
+      display: block;
+      margin: auto;
+      width: 25%;
+
+      &:hover {
+        width: 50%;
+      }
+    }
+    figcaption {
+      font-size: 10px;
+    }
+  }
+  p {
+    color: #040093;
+    display: block
+    margin: auto;
+    padding: 10px;
+  }
+`;
+
 
 export default function Card( {title, explanation, imageURL, date} ) {
     console.log(title);
     return (
-      <div className="card">
+      <StyledCard className="card">
       <h2>{title}</h2>
       <figure>
-        <p>{explanation}</p>
         <img src={imageURL} />
         <figcaption>Image taken on {date}.</figcaption>
       </figure>
-      </div>
+      <p>{explanation}</p>
+      </StyledCard>
     );
 };
